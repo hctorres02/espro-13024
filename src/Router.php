8575716165cd5420644a7245c5dfa7fa9682f081
HC Torres <hctorres02@gmail.com>
2023-03-27 16:11:37 -0300

    Simplifica Router: aplica singleton

--- src/Router.php
+++ src/Router.php
@@ -4,40 +4,23 @@ declare(strict_types=1);
 
 namespace App;
 
-use Laminas\Diactoros\ServerRequest;
-use Laminas\Diactoros\ServerRequestFactory;
-use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
 use League\Route\Router as LeagueRouter;
 
 class Router
 {
-    private ServerRequest $request;
+    private static ?LeagueRouter $router = null;
 
-    private LeagueRouter $router;
-
-    private SapiEmitter $emitter;
-
-    public function __construct()
+    private function __construct()
     {
-        $this->router = new LeagueRouter;
-        $this->emitter = new SapiEmitter;
-        $this->request = ServerRequestFactory::fromGlobals($_SERVER, $_GET, $_POST, $_COOKIE, $_FILES);
+        //
     }
 
-    public function dispatch()
+    public static function getInstance(): LeagueRouter
     {
-        $response = $this->router->dispatch($this->request);
+        if (self::$router === null) {
+            self::$router = new LeagueRouter;
+        }
 
-        $this->emitter->emit($response);
-    }
-
-    public function get(string $path, $handler)
-    {
-        return $this->router->map('GET', $path, $handler);
-    }
-
-    public function post(string $path, $handler)
-    {
-        return $this->router->map('POST', $path, $handler);
+        return self::$router;
     }
 }
