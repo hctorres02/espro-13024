2dbfc95a49cd99a0153314eabf8a25d433bf3ddd
HC Torres <hctorres02@gmail.com>
2023-04-01 09:36:24 -0300

    Adiciona HomeController

--- src/routes.php
+++ src/routes.php
@@ -3,9 +3,12 @@
 declare(strict_types=1);
 
 use App\Controllers\ErrorController;
+use App\Controllers\HomeController;
 use App\Router;
 
 $router = Router::getInstance();
+
+$router->get('/', HomeController::class);
 
 $router->get('/erro403', [ErrorController::class, 'error403']);
 $router->get('/erro404', [ErrorController::class, 'error404']);
